8fd82b410eeed496176a15cd12e9dc0cd3aa5127
shapnesh singh tiwari <shapnesht@gmail.com>
2023-05-26 15:38:46 +0530

    checking authenticaton

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,29 +1,31 @@
-const CustomAPIError = require("../errors");
-const { isTokenValid } = require("../utils");
+const CustomAPIError = require('../errors')
+const { isTokenValid } = require('../utils')
 
 const authenticationHandler = async (req, res, next) => {
-  const token = req.signedCookies.token;
+  const token = req.signedCookies.token
+  console.log(req)
+  console.log(token)
   if (!token) {
-    throw new CustomAPIError.UnauthenticatedError("Authentication invalid");
+    throw new CustomAPIError.UnauthenticatedError('Authentication invalid')
   }
   try {
-    const { name, userId, role } = isTokenValid(token);
-    req.user = { name, userId, role };
-    next();
+    const { name, userId, role } = isTokenValid(token)
+    req.user = { name, userId, role }
+    next()
   } catch (error) {
-    throw new CustomAPIError.UnauthenticatedError("Authentication invalid");
+    throw new CustomAPIError.UnauthenticatedError('Authentication invalid')
   }
-};
+}
 
 const authorizeUser = (...roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
       throw new CustomAPIError.UnauthorizedError(
-        "Unauthorized to access this route"
-      );
+        'Unauthorized to access this route'
+      )
     }
-    next();
-  };
-};
+    next()
+  }
+}
 
-module.exports = { authenticationHandler, authorizeUser };
+module.exports = { authenticationHandler, authorizeUser }
